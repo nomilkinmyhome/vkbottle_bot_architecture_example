ca9c70db48f537a0fe35bdd2d92d7b4e16dcc26a
ichetiva <feoktistovvladislav@outlook.com>
2021-07-02 19:41:01 +0300

    Added simple callback keyboard

---
+++ src/blueprints/simple_keyboard.py
@@ -0,0 +1,40 @@
+import random
+
+from vkbottle import Keyboard, KeyboardButtonColor, Callback
+from vkbottle.bot import Blueprint, Message
+from vkbottle_types import GroupTypes
+from vkbottle_types.events import GroupEventType
+
+bp = Blueprint()
+
+# vbml_ignore_case включаем чтобы бот обрабатывал сообщение в любом регистре
+# Например: если этот параметр будет False, то бот поймет только "!число", а если написать слово
+# С заглавной буквы, то он уже не поймёт (!Число)
+# Но если будет равен True, то бот поймет и "!ЧИСЛО", и "!ЧиСлО", и т.д.
+bp.labeler.vbml_ignore_case = True
+
+random_number_keyboard = (
+    Keyboard(one_time=False, inline=True)
+    .add(Callback("Еще число!", payload={"cmd": "more_numbers"}), color=KeyboardButtonColor.POSITIVE)
+    .get_json()
+)
+
+
+@bp.on.message(text=["!число"])
+async def random_number(message: Message):
+    number = random.randint(1, 100)
+
+    await message.answer(f"Выпало число — {number}!", keyboard=random_number_keyboard)
+
+
+@bp.on.raw_event(GroupEventType.MESSAGE_EVENT, dataclass=GroupTypes.MessageEvent)
+async def message_event(event: GroupTypes.MessageEvent):
+    if event.object.payload["cmd"] == "more_numbers":
+        number = random.randint(1, 100)
+
+        await bp.api.messages.edit(
+            peer_id=event.object.peer_id,
+            conversation_message_id=event.object.conversation_message_id,
+            message=f"Выпало число — {number}!",
+            keyboard=random_number_keyboard
+        )
